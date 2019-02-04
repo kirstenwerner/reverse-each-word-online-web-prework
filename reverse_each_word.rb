f38b6f1a0159a57acb4cbd26bd8f994ae4a021ce
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-04 23:09:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reverse_array = []
-  array.each {|word| reverse_array << word.reverse}
-  reverse_array.join(" ")
+  new_array = array.collect {|word| word.reverse}
+  new_array.join(" ")
 end
